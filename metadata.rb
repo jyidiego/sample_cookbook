b715fa5c665c18f1ef09101046759e5d61396ccb
John Yi <jyi@webmd.net>
2013-05-19 14:05:03 -0400

    Creating test git repo.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'sample_cookbook'
+maintainer       'WebMD'
+maintainer_email 'jyi@webmd.net'
+license          'All rights reserved'
+description      'Installs/Configures sample_cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
